a11bb238bdfffeb5cebe4d4147179c7418c90e57
wiederda <christian.burau@me.com>
2024-09-24 10:10:39 +0200

    Update

--- ubuntu/openssl3_1_win.sh
+++ ubuntu/openssl3_1_win.sh
@@ -27,14 +27,14 @@ mkdir -p $temp/$Out/
 chmod -R ugo+rwx $Home/$tmp
 chmod -R ugo+rwx $temp
 
-tar xzfv $Home/$latest_version.tar.gz
+tar xzfv $Home/openssl-$latest_version.tar.gz
 
-chmod -R ugo+rwx $Home/$latest_version
+chmod -R ugo+rwx $Home/openssl-$latest_version
 
-cd $Home/$latest_version
+cd $Home/openssl-$latest_version
 
-chmod +x $Home/$latest_version/Configure
-$Home/$latest_version/Configure --cross-compile-prefix=x86_64-w64-mingw32- mingw64 --prefix=$Home/$tmp --openssldir=$Home/$tmp
+chmod +x $Home/openssl-$latest_version/Configure
+$Home/openssl-$latest_version/Configure --cross-compile-prefix=x86_64-w64-mingw32- mingw64 --prefix=$Home/$tmp --openssldir=$Home/$tmp
 
 make
 make install
@@ -43,9 +43,9 @@ cd $temp
 
 mkdir $Out
 cp -R $Home/$tmp/bin/ $Out/
-cp $Home/$latest_version/LICENSE.txt $Out/
-cp $Home/$latest_version/NEWS.md $Out/
-cp $Home/$latest_version/CHANGES.md $Out/
+cp $Home/openssl-$latest_version/LICENSE.txt $Out/
+cp $Home/openssl-$latest_version/NEWS.md $Out/
+cp $Home/openssl-$latest_version/CHANGES.md $Out/
 
 zip -r $latest_version.zip $Out
 cp $latest_version.zip $Pfad
@@ -57,5 +57,5 @@ if [ -n "$GOTIFY_SERVER" ]; then
     curl -X POST $GOTIFY_SERVER \
      -H "Content-Type: application/json" \
      -H "X-Gotify-Key: $GOTIFY_KEY" \
-     -d "{\"title\": \"Notification Title\", \"message\": \"$latest_version wurde erfolgreich erstellt\"}"
+     -d "{\"title\": \"Notification Title\", \"message\": \"OpenSSL $latest_version wurde erfolgreich erstellt\"}"
 fi
